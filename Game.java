30770fddb884d24a232e09ced548e30a274647da
zerkfr <zachsherwood111@gmail.com>
2024-10-22 22:46:43 -0500

    added player death

--- Game.java
+++ Game.java
@@ -39,7 +39,7 @@ public class Game extends Canvas implements Runnable{
 		this.addKeyListener(new KeyInput(handler, this));
 		
 		handler.addObject(player); 
-		for(int i = 1; i>0; i--) {
+		for(int i = 5; i>0; i--) {
 			handler.addObject(new Enemy(r.nextInt(WIDTH-50), r.nextInt(HEIGHT-50), ID.Enemy, handler)); 
 		}
 		handler.addObject(new TrackingEnemy(r.nextInt(WIDTH-50), r.nextInt(HEIGHT-50), ID.TrackingEnemy, handler)); 
--- Player.java
+++ Player.java
@@ -34,6 +34,7 @@ public class Player extends GameObject{
 			GameObject tempObject = handler.object.get(i);
 			if(tempObject.getId() == ID.Enemy || tempObject.getId() == ID.Enemy || tempObject.getId() == ID.TrackingEnemy) {
 				if(getBounds().intersects(tempObject.getBounds())) {  //If we intersect with enemy rectangle bounds, do this 
+					handler.removeObject(this);
 					System.out.println("COLLISION!");
 					
 				}
